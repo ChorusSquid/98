c17ee7c5ed6dd521aa6567403216c55e5029cd27
ChorusSquid <toxictyranrex@yahoo.com>
2018-04-16 19:51:32 +0100

    Update 98.py

--- 98.py
+++ 98.py
@@ -154,10 +154,14 @@ def get_images(url, card, rat = None):
                 links.remove(pack)
             else:
                 posts.append(pack)
-    if not posts:       
-        return links[0].replace("/scale-to-width-down/305", "").replace("/scale-to-width-down/220", "")
-    else:       
-        return posts[0].replace("/scale-to-width-down/305", "").replace("/scale-to-width-down/220", "")
+    if not posts:
+        if "scale-to-width-down/" in links[0]:
+            links[0] = links[0][0:104] + links[0][127:]
+        return links[0]
+    else:
+        if "scale-to-width-down/" in posts[0]:
+            posts[0] = posts[0][0:104] + posts[0][127:]
+        return posts[0]
 
 @nine.event
 async def on_ready():
